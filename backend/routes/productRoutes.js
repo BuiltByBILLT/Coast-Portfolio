bde908b966952a36d6b3b2166e859257cae5814c
Brandon <brandon@builtbybillt.com>
2021-05-10 21:43:36 -0500

    Express Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
 
 const router = express.Router()
-import Product from '../models/productModel.js'
 
 // @desc Fetch all products
 // @route GET /api/products
@@ -20,7 +20,8 @@ router.get('/:id', asyncHandler(async (req, res) => {
     if (product) {
         res.json(product)
     } else {
-        res.status(404).json({ message: 'Product not found' })
+        res.status(404)
+        throw new Error('Product not found')
     }
 }))
 
