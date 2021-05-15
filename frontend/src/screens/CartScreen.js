65f11e46fc9fd04b4617c5e2eff3cf6cbc264b9a
Brandon <brandon@builtbybillt.com>
2021-05-14 21:00:04 -0500

    Cart Screen function

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToCart } from '../actions/cartActions'
 
-const CartScreen = () => {
+import Message from '../components/Message'
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem, Form } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+
+const CartScreen = ({ match, location, history }) => {
+    const productId = match.params.id
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+        return () => { }
+    }, [dispatch, productId, qty])
+
     return (
         <div>
             Cart
