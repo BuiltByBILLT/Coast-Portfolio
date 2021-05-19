43bf73b1ce549587f9912d50bd73fd3570a28d6c
Brandon <brandon@builtbybillt.com>
2021-05-18 18:22:29 -0700

    added Register User

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,40 @@ import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 
+// @desc Register a new user
+// @route POST /api/user
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email: email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
+    }
+})
+
+
 // @desc Auth user& get token
 // @route POST /api/user/login
 // @access Public
@@ -45,4 +79,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { registerUser, authUser, getUserProfile }
