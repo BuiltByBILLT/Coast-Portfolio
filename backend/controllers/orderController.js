84f3918182ad6613f5c6bb21833031ba07d2fd7c
Brandon <brandon@builtbybillt.com>
2021-05-22 22:59:21 -0700

    Show Orders in Profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -74,5 +74,15 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 })
 
+// @desc Get logged in user orders
+// @route GET /api/orders/myorders
+// @access Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id })
+    res.json(orders)
 
-export { addOrderItems, getOrderByID, updateOrderToPaid }
+})
+
+
+
+export { addOrderItems, getOrderByID, updateOrderToPaid, getMyOrders }
