5f5c28be7990a93ed109383e64107ab45df96d48
Brandon <brandon@builtbybillt.com>
2021-06-25 10:56:10 -0500

    Search Bar and Pagination

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,33 +3,47 @@ import { Col, Row } from 'react-bootstrap'
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { listProducts, resetProducts } from '../actions/productActions'
 
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+    const keyword = match.params.keyword
+
+    const pageNumber = match.params.pageNumber || 1
+
     const dispatch = useDispatch()
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
-        dispatch(listProducts())
+        dispatch(listProducts(keyword, pageNumber))
         return dispatch(resetProducts())
-    }, [dispatch])
+    }, [dispatch, keyword, pageNumber])
 
     return (
         <>
             <h1>Latest Products</h1>
             {loading ? <Loader />
                 : error ? (<Message variant='danger'>{error}</Message>)
-                    : (<Row>
-                        {products.map(product => (
-                            <Col className='align-items-stretch d-flex' key={product._id} sm='12' md='6' lg='4' xl='3'>
-                                <Product product={product} />
-                            </Col>
-                        ))}
-                    </Row>)
+                    : (
+                        <>
+                            <Row>
+                                {products.map(product => (
+                                    <Col className='align-items-stretch d-flex' key={product._id} sm='12' md='6' lg='4' xl='3'>
+                                        <Product product={product} />
+                                    </Col>
+                                ))}
+                            </Row>
+                            <Paginate
+                                pages={pages}
+                                page={page}
+                                keyword={keyword ? keyword : ''}
+                            />
+                        </>
+                    )
             }
         </>
     )
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,15 +4,17 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import Paginate from '../components/Paginate'
 import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 const ProductListScreen = ({ history, match }) => {
+    const pageNumber = match.params.pageNumber || 1
 
     const dispatch = useDispatch()
 
     const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+    const { loading, error, products, page, pages } = productList
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
@@ -40,10 +42,10 @@ const ProductListScreen = ({ history, match }) => {
         if (successCreate) {
             history.push(`/admin/product/${createdProduct._id}/edit`)
         } else {
-            dispatch(listProducts())
+            dispatch(listProducts('', pageNumber))
         }
         return () => { }
-    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct, pageNumber])
 
     const deleteHandler = (id) => {
         if (window.confirm('Are you sure')) {
@@ -70,7 +72,7 @@ const ProductListScreen = ({ history, match }) => {
             {loadingCreate && <Loader />}
             {errorCreate && <Message variant='danger'>{error}</Message>}
             {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message>
-                : (
+                : (<>
                     <Table striped bordered hover responsive className='table-sm'>
                         <thead>
                             <tr>
@@ -105,6 +107,8 @@ const ProductListScreen = ({ history, match }) => {
                             ))}
                         </tbody>
                     </Table>
+                    <Paginate pages={pages} page={page} isAdmin={true} />
+                </>
                 )}
         </>
     )
