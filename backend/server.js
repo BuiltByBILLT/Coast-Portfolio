f86d2b0b6c38f66a418a73e63f147844cd1b1daa
Brandon <brandon@builtbybillt.com>
2021-12-27 10:15:56 -0800

    serve prodimage with express static

--- backend/server.js
+++ backend/server.js
@@ -53,11 +53,11 @@ app.use('/api/shipping', shippingRoutes)
 app.use('/api/tax', avaTaxRoutes)
 
 // app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
-// app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
 const __dirname = path.resolve()
 if (process.env.NODE_ENV === 'production') {
+    app.use('/prodimages', express.static(path.join(__dirname, 'frontend', 'public', 'prodimages')))
     app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 } else {
